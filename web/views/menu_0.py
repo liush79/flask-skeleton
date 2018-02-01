2e90ed7ac28b8b9409a77cb5a2080ec0106f8861
seungho.ryu <98koom@hanmail.net>
2018-02-01 23:41:57 +0900

    menu_0 sample page is added.

--- web/views/menu_0.py
+++ web/views/menu_0.py
@@ -8,10 +8,9 @@ bp = Blueprint('menu_0', __name__, url_prefix='/menu_0')
 @bp.route('/', methods=['GET'])
 def view_dashboard():
     try:
-        render_data = {
-
+        sample_data = {
         }
-        return render_template('template_menu_0/dashboard.html', render_data=render_data)
+        return render_template('template_menu_0/menu_0_dashboard.html', render_data=sample_data)
     except Exception, e:
         print 'Exception,', str(e)
         abort(500)
